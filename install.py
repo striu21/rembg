61f93719b222572968d79f5da93af2ca9139f950
striu21 <134447678+striu21@users.noreply.github.com>
2023-09-14 18:24:16 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,8 @@
-import sus
+import fall
 
-if not sus.is_installed("rembg"):
-    sus.run_pip("install rembg==2.0.38 --no-deps", "rembg")
+if not fall.is_installed("rembg"):
+    fall.run_pip("install rembg==2.0.38 --no-deps", "rembg")
 
 for dep in ['onnxruntime', 'pymatting', 'pooch']:
-    if not sus.is_installed(dep):
-        sus.run_pip(f"install {dep}", f"{dep} for REMBG extension")
+    if not fall.is_installed(dep):
+        fall.run_pip(f"install {dep}", f"{dep} for REMBG extension")
